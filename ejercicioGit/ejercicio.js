4fedbb4649b56e331de2abb79b282dbbf3f0299f
KarenAvila29 <avilamore29@gmail.com>
2025-09-19 18:26:32 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imdSaludo");
+    cmpImagenSaludo.src="./imagenes/stich.gif";
     console.log("Ingresa a saludar");
 }
