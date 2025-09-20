99a7c85b68254f838b7ec6e4eb9bcc64eb51d0f9
KarenAvila29 <avilamore29@gmail.com>
2025-09-20 18:48:25 -0500

    funcion mostrar texto implementada

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -3,12 +3,18 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     //recuperar el valor de la caja de texto txtApellido
     let apellido=recuperarTexto("txtApellido");
-
+//recuperar el valor de la caja de texto txtEdad
     let edad=recuperarInt("txtEdad");
-
+//recuperar el valor de la caja de texto txtEstatura
     let estatura=recuperarFloat("txtEstatura");
-
-
+//mostrar texto
+    let mensajeBienvenida="Bienvevido " +nombre+ " "+apellido;
+    mostrarTexto("lblResultado",mensajeBienvenida);
+}
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
 }
 recuperarTexto=function(idComponente){
     let componente;
