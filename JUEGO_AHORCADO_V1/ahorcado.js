0a0f054e2ceff155cc798c884ec231bf9d0c20d9
KarenAvila29 <avilamore29@gmail.com>
2025-10-02 01:15:59 -0500

    PASO1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,16 +1,31 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
 guardarPalabra=function(){
+    let resultado = true;
    let palabra= recuperarTexto("txtSecreta");
-   if((esMayuscula(palabra)==false)){
-        alert(" DEBE INGRESAR UNA PALABRA MAYUSCULA");
+    if(palabra.length !==5){
+        alert(" DEBE INGRESAR UNA PALABRA MAYUSCULA DE 5 LETRAS");
+        return;
    }
-
+    for(i=0;i<palabra.length;i++){
+  
+        let variable= palabra.charAt(i); 
+        if(esMayuscula(variable)==false){
+            resultado=false;
+            break;
+   }
 }
-
-
+   if(!resultado){
+       alert(" DEBE INGRESAR UNA PALABRA MAYUSCULA DE 5 LETRAS");
+      }  
+   else{
+        palabraSecreta=palabra;
+        
+   }
+  }  
 esMayuscula=function(caracter){
     let codigoLetra=caracter.charCodeAt(caracter);
-    if(codigoLetra>=65 && codigoLetra<=90 /*|| codigoLetra==209*/){  
+    if(codigoLetra>=65 && codigoLetra<=90 ){  
         return true;
     }else{
         return false;
