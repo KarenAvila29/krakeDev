6f04c71c8a6a9cdc65f7532c982d7bf0fcd809d9
KarenAvila29 <avilamore29@gmail.com>
2025-10-01 20:40:48 -0500

    RETO 37

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,27 @@
+mostrarNumeros=function(){
+    console.log("Antes del for");
+    for(let i=0;i<4;i++){
+        console.log(i);
+
+    }
+
+    console.log("despues del for");
+}
+mostrarNumeros2=function(){
+    console.log("Antes del for");
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+
+    }
+
+    console.log("despues del for");
+}
+mostrarImpares=function(){
+    console.log("Antes del for");
+    for(let i=1;i<=21;i+=2){
+        console.log(i);
+    }
+
+    console.log("despues del for");
+
+}
