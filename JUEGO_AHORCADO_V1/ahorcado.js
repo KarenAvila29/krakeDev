7029707fa0e3cdfe80ac34702c596ab0d0ed1fa5
KarenAvila29 <avilamore29@gmail.com>
2025-10-02 20:00:30 -0500

    PASO 3

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -22,13 +22,36 @@ guardarPalabra=function(){
         palabraSecreta=palabra;
    }
   }  
-  guardarLetra=function(letra,posicion){
+  ingresarLetra=function(){
+    let letra1= recuperarTexto("txtLetra");
+        if(esMayuscula(letra1)==true){
+            validar(letra1);
+        }else{
+             alert(" SOLO SE ACEPTAN MAYUSCULAS");
+        }
+  }
+  mostrarLetra=function(letra,posicion){
     if (posicion >= 0 && posicion <= 4) {
         let div = document.getElementById("div" + posicion);
         if (div) {
           div.textContent = letra;
         }
       }
+  }
+  validar=function(letra){
+    let letrasEncontradas=0;
+     for (let i = 0; i < palabraSecreta.length; i++) {
+        // Verifica si coincide con la letra que recibe como parámetro.
+        if (palabraSecreta[i] == letra) {
+            // 3. Si son iguales llama a mostrarLetra
+            mostrarLetra(letra, i); 
+            
+            // Además incrementar la variable letrasEncontradas en 1.
+            letrasEncontradas++;
+        }
+    }
+
+
   }
 esMayuscula=function(caracter){
     let codigoLetra=caracter.charCodeAt(caracter);
